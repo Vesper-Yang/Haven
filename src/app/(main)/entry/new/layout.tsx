8fb7f602e5c75e4338f091adf8e77b002b3a244f
Vesper-Yang <1105061841@qq.com>
2025-09-12 11:27:42 +0800

    搭建new entry的框架(还未填充内容)

---
+++ src/app/(main)/entry/new/layout.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React, { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+
+const EntryLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <div>
+      <Link href="/home" className="mb-10">
+        <Button>Back</Button>
+      </Link>
+      <Suspense fallback={<BarLoader color="orange" width={"100%"} />}>
+        {children}
+      </Suspense>
+    </div>
+  );
+};
+
+export default EntryLayout;
